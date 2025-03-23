ea5d46dd04ee791921abaa970fe7bcbae9d8373c
Liam Hall <40840255+wearethreebears@users.noreply.github.com>
2025-03-23 09:41:02 +0000

    Add textarea

---
+++ dist/components/forms/Textarea/Textarea.types.ts
@@ -0,0 +1,34 @@
+import type { IDefaultTokens, TPercent, TRem } from "~/DesignTokens.types";
+import type { TIcon } from "@ohto/core/dist/components/base/Icon/Icon.types";
+
+export type TTextareaAutoComplete = "on" | "off" | string;
+
+export interface ITextareaProps {
+  id: string;
+  label: string;
+  hideLabel?: boolean;
+  placeholder?: string;
+  name: string;
+  error?: string;
+  value?: string;
+  class?: any;
+  autocomplete?: TTextareaAutoComplete;
+  testId?: string;
+  icon?: TIcon;
+  required?: boolean;
+}
+
+export interface ITextareaCustomTokens {
+  textareaWidth: TPercent;
+  textareaHeight: TRem;
+}
+
+export type TTextareaStaticTokens = Pick<
+  IDefaultTokens & ITextareaCustomTokens,
+  | "paddingY"
+  | "paddingX"
+  | "typographyColor"
+  | "placeholder__typographyColor"
+  | "textareaWidth"
+  | "textareaHeight"
+>;
